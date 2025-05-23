ac22c2bc83e88dfc37c0ccbf3d4753e1a71ea6cc
Carolyne Ndunge <84581938+carolynen@users.noreply.github.com>
2025-05-24 02:20:43 +0300

    add Atendee model for managing event attendees

---
+++ app/Models/Atendee.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Atendee extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'user_id',
+        'event_id'
+    ];
+
+    /**
+     * The user that is attending the event.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    /**
+     * The event that the user is attending.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+}
