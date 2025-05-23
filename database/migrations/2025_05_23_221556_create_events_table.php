6cc1a751aef3d5644ccd8e5b8c14c965f55f0d51
Carolyne Ndunge <84581938+carolynen@users.noreply.github.com>
2025-05-24 02:17:03 +0300

    create migration for events table with necessary fields

---
+++ database/migrations/2025_05_23_221556_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name'); //name of the event
+            $table->text('description'); //description of the event
+            $table->dateTime('start_time'); //start time of the event
+            $table->dateTime('end_time'); //end time of the event
+
+            $table->foreignIdFor(User::class); //
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
