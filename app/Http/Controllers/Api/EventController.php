5c78e3138dc3cdbf3e143d06f34256e75eb36aea
Carolyne Ndunge <84581938+carolynen@users.noreply.github.com>
2025-10-23 12:34:54 +0300

    Refactor EventController to implement event creation, updating, and deletion functionality

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
@@ -20,7 +21,24 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after:start_date',
+        ]);
+
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_date' => 'required|date',
+                'end_date' => 'required|date|after:start_date',
+            ]),
+            'user_id' => 1,
+        ]);
+
+        return $event;
     }
 
     /**
@@ -34,16 +52,30 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update ([
+            ...$request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_date' => 'sometimes|date',
+                'end_date' => 'sometimes|date|after:start_date',
+            ]),
+            'user_id' => 1,
+        ]);
+
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        return response()->json(
+            'message', 'Event deleted successfully.'
+        );
     }
 }
